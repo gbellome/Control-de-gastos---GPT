a85f4d2a34c61b584e479ed4823ca5c5cb6388a6
Gabi <bellome.gabriel@gmail.com>
2025-01-21 09:32:54 -0300

    Update CreateRot because render is deprecated

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,17 @@
 // src/index.tsx
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import App from "./App";
 import dotenv from 'dotenv';
 
+// Inicializo todas las variables de entorno según el entorno iniciado
 dotenv.config()
 
 // Renderiza el componente principal de la aplicación en el elemento con id 'root'
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('root')
+
+createRoot(container!).render(
+<React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>
+)
